b2f93234fa61bbfd9578666b89049ea2e8689254
Usuario <javichua.a@hotmail.com>
2022-07-07 14:39:46 +0200

    feat:add resetpassword activity and movement between activitys

--- app/src/main/java/com/example/inventoryscout/login.java
+++ app/src/main/java/com/example/inventoryscout/login.java
@@ -2,7 +2,9 @@ package com.example.inventoryscout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void changeToResetPassword(View view){
+        Intent i = new Intent(this,resetpassword.class);
+        startActivity(i);
     }
 }
