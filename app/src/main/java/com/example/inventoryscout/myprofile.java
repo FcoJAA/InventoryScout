a7f00e249660018b32f96e017a0e2fc6c92505ea
Usuario <javichua.a@hotmail.com>
2022-07-11 13:12:56 +0200

    feat: new buttons

--- app/src/main/java/com/example/inventoryscout/myprofile.java
+++ app/src/main/java/com/example/inventoryscout/myprofile.java
@@ -2,13 +2,36 @@ package com.example.inventoryscout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class myprofile extends AppCompatActivity {
+
+    public FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myprofile);
+    }
+
+    //MenuBar
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menubar1,menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.option1){
+            Intent i = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(i);
+            mAuth.signOut();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
