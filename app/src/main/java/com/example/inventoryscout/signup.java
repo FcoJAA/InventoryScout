125e44667544417ab99111055a06a567847b21a4
Usuario <javichua.a@hotmail.com>
2022-07-11 12:14:54 +0200

    feat: add auth code

--- app/src/main/java/com/example/inventoryscout/signup.java
+++ app/src/main/java/com/example/inventoryscout/signup.java
@@ -1,14 +1,82 @@
 package com.example.inventoryscout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class signup extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText email,password,password2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.et1sigup);
+        password = findViewById(R.id.et2sigup);
+        password2 = findViewById(R.id.et3sigup);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    public void registerUser(View view){
+
+        if( password.length() > 8){
+            if(password.getText().toString().equals(password2.getText().toString())) {
+                mAuth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString())
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {  //SI EL REGISTRO ES CORRECTO
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(getApplicationContext(), "Cuenta creada.", Toast.LENGTH_SHORT).show();
+                                    //Vuelta al inicio
+                                    Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                                    startActivity(i);
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "Error en la creacion de la cuenta. ", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }else {
+                Toast.makeText(this, "Error en la coincidencia de contraeñas.", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(this, "Tamaño deficiente de la contraseña.", Toast.LENGTH_SHORT).show();
+        }
+
+        /* mAuth.signInWithCustomToken()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(signup.this, "Error de Registro", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });*/
+    }
+    //FirebaseAuth.getInstance().signOut();
 }
