cd6c1395b12a5f949098f301998f51d7c8609ed6
Usuario <javichua.a@hotmail.com>
2022-07-11 12:57:28 +0200

    feat: add menubar and log-out

--- app/src/main/java/com/example/inventoryscout/MainActivity.java
+++ app/src/main/java/com/example/inventoryscout/MainActivity.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    public FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +29,21 @@ public class MainActivity extends AppCompatActivity {
     public void changeToSignUp(View view){
         Intent i = new Intent(this,signup.class);
         startActivity(i);
+    }
+
+    //MenuBar
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menubar1,menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.option1){
+            Intent i = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(i);
+            mAuth.signOut();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
