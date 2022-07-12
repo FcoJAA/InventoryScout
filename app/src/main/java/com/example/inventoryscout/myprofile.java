fd89b1977aed896c785842d9bc64a7b91b151784
Usuario <javichua.a@hotmail.com>
2022-07-12 10:58:23 +0200

    feat: add insert-items screen

--- app/src/main/java/com/example/inventoryscout/myprofile.java
+++ app/src/main/java/com/example/inventoryscout/myprofile.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -17,6 +18,11 @@ public class myprofile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myprofile);
+    }
+
+    public void insertItem(View view){
+        Intent i = new Intent(this,insertItems.class);
+        startActivity(i);
     }
 
     //MenuBar
