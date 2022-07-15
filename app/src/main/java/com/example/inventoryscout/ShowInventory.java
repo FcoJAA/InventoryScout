2e9f1188750b498a4fc5af739fdd294ab01adb23
Usuario <javichua.a@hotmail.com>
2022-07-15 13:03:01 +0200

    feat: showitems coden

--- app/src/main/java/com/example/inventoryscout/ShowInventory.java
+++ app/src/main/java/com/example/inventoryscout/ShowInventory.java
@@ -17,8 +17,8 @@ public class ShowInventory extends AppCompatActivity {
 
     static FirebaseFirestore db = FirebaseFirestore.getInstance();
     final String colletionpath1 = "MainInventory";
-    public TextView tvshow1, tvshow2,tvshow3;
-    public String[] items = new String[]{"Tornillos", "Tuercas", "Tornillo B"};
+    public TextView tvshow1, tvshow2,tvshow3,tvshow4,tvshow5,tvshow6,tvshow7,tvshow8,tvshow9;
+    public String[] items = new String[]{"Tornillos", "Tuercas","Tuberias Acero","Tornillo B","Lija","Estropajo","Destornillador de Estrella","Arandela","Cinta Americana"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +28,14 @@ public class ShowInventory extends AppCompatActivity {
         tvshow1 = (TextView) findViewById(R.id.tvShow1);
         tvshow2 = (TextView) findViewById(R.id.tvShow2);
         tvshow3 = (TextView) findViewById(R.id.tvShow3);
+        tvshow4 = (TextView) findViewById(R.id.tvShow4);
+        tvshow5 = (TextView) findViewById(R.id.tvShow5);
+        tvshow6 = (TextView) findViewById(R.id.tvShow6);
+        tvshow7 = (TextView) findViewById(R.id.tvShow7);
+        tvshow8 = (TextView) findViewById(R.id.tvShow8);
+        tvshow9 = (TextView) findViewById(R.id.tvShow9);
 
-        for (int i = 0; i <3;i++) {
+        for (int i = 0; i <9;i++) {
             mostrar(items[i]);
         }
     }
@@ -45,7 +51,13 @@ public class ShowInventory extends AppCompatActivity {
                     if (document.exists()) {
                         if(value.equals("Tornillos")){tvshow1.setText(document.get("Unidades").toString());}
                         if(value.equals("Tuercas")){tvshow2.setText(document.get("Unidades").toString());}
-                        if(value.equals("Tornillo B")){tvshow3.setText(document.get("Unidades").toString());}
+                        if(value.equals("Tuberias Acero")){tvshow3.setText(document.get("Unidades").toString());}
+                        if(value.equals("Tornillo B")){tvshow4.setText(document.get("Unidades").toString());}
+                        if(value.equals("Lija")){tvshow5.setText(document.get("Unidades").toString());}
+                        if(value.equals("Estropajo")){tvshow6.setText(document.get("Unidades").toString());}
+                        if(value.equals("Destornillador de Estrella")){tvshow7.setText(document.get("Unidades").toString());}
+                        if(value.equals("Arandela")){tvshow8.setText(document.get("Unidades").toString());}
+                        if(value.equals("Cinta Americana")){tvshow9.setText(document.get("Unidades").toString());}
 
                     } else {
                         Toast.makeText(ShowInventory.this, "Error", Toast.LENGTH_SHORT).show();
